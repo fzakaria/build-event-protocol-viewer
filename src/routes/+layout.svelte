d712934cfc36b464553a1663afa49f8ca2223ccc
Farid Zakaria <farid.m.zakaria@gmail.com>
2025-01-24 22:20:40 -0800

    First commit!

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  // Import bootstrap
+  // https://stackoverflow.com/a/75272446
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import 'bootstrap/dist/js/bootstrap.min.js';
+  import '../app.css';
+
+  import { state } from './store.svelte';
+
+  let { children } = $props();
+</script>
+
+<div class="container">
+  <header class="d-flex justify-content-center border-bottom mb-4 flex-wrap py-3">
+    <a
+      href="/"
+      class="d-flex align-items-center mb-md-0 me-md-auto link-body-emphasis text-decoration-none mb-3"
+    >
+      <img class="bi me-2" width="40" height="32" src="/bazel.svg" alt="" />
+      <span class="fs-4">Bazel Build Event Protocol</span>
+    </a>
+
+    <div class="mb-3">
+      <input bind:files={state.files} class="form-control" type="file" id="formFile" />
+    </div>
+  </header>
+</div>
+
+<div class="container">
+  {@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { state } from './store.svelte';
+  import { load, Reader } from 'protobufjs';
+  import "@andypf/json-viewer"
+
+  let file = $derived(state.files?.[0]);
+  let error: string | null = null;
+  let events = $derived(parseProto(file));
+
+  async function parseProto(file: File | undefined) {
+    if (!file) return null;
+    const bes_proto_file = await load('/build_event_stream.proto');
+    const BuildEventType = bes_proto_file.lookupType('build_event_stream.BuildEvent');
+    console.log(file instanceof Blob);
+    const data = await file.arrayBuffer();
+    let reader = new Reader(new Uint8Array(data));
+    const events = [];
+    while (reader.pos < reader.len) {
+      const event = BuildEventType.decodeDelimited(reader);
+      events.push(event.toJSON());
+    }
+    return events;
+  }
+
+  async function loadSampleFile() {
+    const response = await fetch('/samples/demo.pb');
+    if (!response.ok) {
+      throw new Error(`Failed to load the sample file: ${response.statusText}`);
+    }
+    const blob = await response.blob();
+    const file = new File([blob], 'demo.pb', { type: blob.type });
+    const dataTransfer = new DataTransfer();
+    dataTransfer.items.add(file);
+    state.files = dataTransfer.files;
+  }
+</script>
+
+<!-- This pattern is copied from https://github.com/sveltejs/svelte/issues/13722#issuecomment-2425182224 -->
+{#if file}
+  {#await events}
+    <div class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  {:then items}
+  <andypf-json-viewer expanded=2 data={ items } show-toolbar="true">
+
+  </andypf-json-viewer>
+  {:catch reason}
+    <div class="alert alert-danger" role="alert">{reason}</div>
+  {/await}
+{:else if error}
+  <div class="alert alert-danger" role="alert">{error}</div>
+{:else}
+  <div class="alert alert-warning" role="alert">Please select a file to view.</div>
+  <button class="btn btn-primary mt-3" onclick={loadSampleFile}> Load Sample File </button>
+{/if}
