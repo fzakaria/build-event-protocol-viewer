15fabc148856acede0078a3c5433ebc419d257fe
Farid Zakaria <fzakaria@confluent.io>
2025-07-31 14:07:23 -0700

    Bump to 25.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     systems.url = "github:nix-systems/default";
   };
 
